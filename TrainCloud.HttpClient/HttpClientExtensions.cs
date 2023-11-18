f3f08574978b0f7f4a9207d841b09a5f37633695
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2023-11-18 16:11:09 +0100

    Streamlined

--- TrainCloud.HttpClient/HttpClientExtensions.cs
+++ TrainCloud.HttpClient/HttpClientExtensions.cs
@@ -1,8 +1,8 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text.Json;
 
 namespace TrainCloud.HttpClient;
-
 
 public static class HttpClientExtensions
 {
@@ -10,86 +10,97 @@ public static class HttpClientExtensions
 
     public delegate void OnError(int statusCode);
 
-    public static async Task<TResponse?> GetJsonAsync<TResponse>(this System.Net.Http.HttpClient client, string requestUri, OnError? onErrorCallback = null)
+    public static void AuthorizeClient(this System.Net.Http.HttpClient client, string token)
+    {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
+
+    private static async Task<TResponse?> ProcessResponseAsync<TResponse>(HttpResponseMessage response, OnError? onErrorCallback = null)
+    {
+        if (!response.IsSuccessStatusCode)
+        {
+            if (onErrorCallback is not null)
+            {
+                onErrorCallback((int)response.StatusCode);
+            }
+            return default;
+        }
+
+        MemoryStream contentStream = (MemoryStream)await response.Content.ReadAsStreamAsync();
+
+        TResponse? model = await JsonSerializer.DeserializeAsync<TResponse>(contentStream, TrainCloudJsonSerializerOptions);
+
+        return model;
+    }
+
+    public static async Task<TResponse?> GetRequestAsync<TResponse>(this System.Net.Http.HttpClient client, string requestUri, OnError? onErrorCallback = null)
     {
         try
         {
             using HttpResponseMessage response = await client.GetAsync(requestUri);
-            if (!response.IsSuccessStatusCode)
-            {
-                if (onErrorCallback is not null)
-                {
-                    onErrorCallback((int)response.StatusCode);
-                }
-                return default;
-            }
 
-            Stream contentStream = await response.Content.ReadAsStreamAsync();
-            TResponse? model = await JsonSerializer.DeserializeAsync<TResponse>(contentStream, TrainCloudJsonSerializerOptions);
+            TResponse? model = await ProcessResponseAsync<TResponse>(response, onErrorCallback);
 
             return model;
         }
-        catch (Exception ex)
+        catch
         {
+            if (onErrorCallback is not null)
+            {
+                onErrorCallback(500);
+            }
+
             return default;
         }
     }
 
-    public static async Task<TResponse?> PostJsonAsync<TPost, TResponse>(this System.Net.Http.HttpClient client, string requestUri, TPost param, OnError? onErrorCallback = null)
+    public static async Task<TResponse?> PostRequestAsync<TPost, TResponse>(this System.Net.Http.HttpClient client, string requestUri, TPost param, OnError? onErrorCallback = null)
     {
         try
         {
             HttpContent parameterContent = JsonContent.Create(param, typeof(TPost));
 
             using HttpResponseMessage response = await client.PostAsync(requestUri, parameterContent);
-            if (!response.IsSuccessStatusCode)
-            {
-                if (onErrorCallback is not null)
-                {
-                    onErrorCallback((int)response.StatusCode);
-                }
-                return default;
-            }
 
-            Stream contentStream = await response.Content.ReadAsStreamAsync();
-            TResponse? model = await JsonSerializer.DeserializeAsync<TResponse>(contentStream, TrainCloudJsonSerializerOptions);
+            TResponse? model = await ProcessResponseAsync<TResponse>(response, onErrorCallback);
 
             return model;
         }
-        catch (Exception ex)
+        catch
         {
+            if (onErrorCallback is not null)
+            {
+                onErrorCallback(500);
+            }
+
             return default;
         }
     }
 
-    public static async Task<TResponse?> PatchJsonAsync<TPatch, TResponse>(this System.Net.Http.HttpClient client, string requestUri, TPatch param, OnError? onErrorCallback = null)
+    public static async Task<TResponse?> PatchRequestAsync<TPatch, TResponse>(this System.Net.Http.HttpClient client, string requestUri, TPatch param, OnError? onErrorCallback = null)
     {
         try
         {
             HttpContent parameterContent = JsonContent.Create(param, typeof(TPatch));
 
             using HttpResponseMessage response = await client.PatchAsync(requestUri, parameterContent);
-            if (!response.IsSuccessStatusCode)
-            {
-                if (onErrorCallback is not null)
-                {
-                    onErrorCallback((int)response.StatusCode);
-                }
-                return default;
-            }
 
-            Stream contentStream = await response.Content.ReadAsStreamAsync();
-            TResponse? model = await JsonSerializer.DeserializeAsync<TResponse>(contentStream, TrainCloudJsonSerializerOptions);
+            TResponse? model = await ProcessResponseAsync<TResponse>(response, onErrorCallback);
 
             return model;
         }
-        catch (Exception ex)
+        catch
         {
+            if (onErrorCallback is not null)
+            {
+                onErrorCallback(500);
+            }
+
             return default;
         }
     }
 
-    public static async Task<bool> DeleteAsyncX(this System.Net.Http.HttpClient client, string requestUri, OnError? onErrorCallback = null)
+    public static async Task<bool> DeleteRequestAsync(this System.Net.Http.HttpClient client, string requestUri, OnError? onErrorCallback = null)
     {
         try
         {
@@ -105,9 +116,14 @@ public static class HttpClientExtensions
 
             return true;
         }
-        catch (Exception ex)
+        catch
         {
-            return false;
+            if (onErrorCallback is not null)
+            {
+                onErrorCallback(500);
+            }
+
+            return default;
         }
     }
 }
