01b3a8face4a5635fae89d361e9ce8282a95c6e9
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2024-06-14 15:23:32 +0200

    Public release

---
+++ README.md
@@ -0,0 +1,58 @@
+# 🚆 TrainCloud.HttpClient
+
+The `TrainCloud.HttpClient` library provides simple extensions for `System.Net.HttpClient` to wrap the de-/serialization and Status/Error handling.
+Theese extensions get used in the `TrainCloud(.Tests).Application.*` and `TrainCloud(.Tests).Microservices.*` projects to make the application run like the tests and the test run like the application.
+
+## Status
+
+### GitHub Actions
+[![SonarQube](https://github.com/traincloud-net/TrainCloud.HttpClient/actions/workflows/sonarqube.yml/badge.svg)](https://github.com/traincloud-net/TrainCloud.HttpClient/actions/workflows/sonarqube.yml) 
+[![NuGet](https://github.com/traincloud-net/TrainCloud.HttpClient/actions/workflows/nuget.yml/badge.svg)](https://github.com/traincloud-net/TrainCloud.HttpClient/actions/workflows/nuget.yml) 
+
+### SonarQube
+[![Bugs](https://sonarqube.traincloud.net/api/project_badges/measure?project=TrainCloud.HttpClient&metric=bugs&token=sqb_2a3368bc906f3ad06a68c26889bb616d1fa59a97)](https://sonarqube.traincloud.net/dashboard?id=TrainCloud.HttpClient) 
+[![Code Smells](https://sonarqube.traincloud.net/api/project_badges/measure?project=TrainCloud.HttpClient&metric=code_smells&token=sqb_2a3368bc906f3ad06a68c26889bb616d1fa59a97)](https://sonarqube.traincloud.net/dashboard?id=TrainCloud.HttpClient) 
+[![Duplicated Lines (%)](https://sonarqube.traincloud.net/api/project_badges/measure?project=TrainCloud.HttpClient&metric=duplicated_lines_density&token=sqb_2a3368bc906f3ad06a68c26889bb616d1fa59a97)](https://sonarqube.traincloud.net/dashboard?id=TrainCloud.HttpClient) 
+[![Lines of Code](https://sonarqube.traincloud.net/api/project_badges/measure?project=TrainCloud.HttpClient&metric=ncloc&token=sqb_2a3368bc906f3ad06a68c26889bb616d1fa59a97)](https://sonarqube.traincloud.net/dashboard?id=TrainCloud.HttpClient) 
+[![Maintainability Rating](https://sonarqube.traincloud.net/api/project_badges/measure?project=TrainCloud.HttpClient&metric=sqale_rating&token=sqb_2a3368bc906f3ad06a68c26889bb616d1fa59a97)](https://sonarqube.traincloud.net/dashboard?id=TrainCloud.HttpClient) 
+[![Reliability Rating](https://sonarqube.traincloud.net/api/project_badges/measure?project=TrainCloud.HttpClient&metric=reliability_rating&token=sqb_2a3368bc906f3ad06a68c26889bb616d1fa59a97)](https://sonarqube.traincloud.net/dashboard?id=TrainCloud.HttpClient) 
+[![Security Hotspots](https://sonarqube.traincloud.net/api/project_badges/measure?project=TrainCloud.HttpClient&metric=security_hotspots&token=sqb_2a3368bc906f3ad06a68c26889bb616d1fa59a97)](https://sonarqube.traincloud.net/dashboard?id=TrainCloud.HttpClient) 
+[![Security Rating](https://sonarqube.traincloud.net/api/project_badges/measure?project=TrainCloud.HttpClient&metric=security_rating&token=sqb_2a3368bc906f3ad06a68c26889bb616d1fa59a97)](https://sonarqube.traincloud.net/dashboard?id=TrainCloud.HttpClient) 
+[![Technical Debt](https://sonarqube.traincloud.net/api/project_badges/measure?project=TrainCloud.HttpClient&metric=sqale_index&token=sqb_2a3368bc906f3ad06a68c26889bb616d1fa59a97)](https://sonarqube.traincloud.net/dashboard?id=TrainCloud.HttpClient) 
+[![Vulnerabilities](https://sonarqube.traincloud.net/api/project_badges/measure?project=TrainCloud.HttpClient&metric=vulnerabilities&token=sqb_2a3368bc906f3ad06a68c26889bb616d1fa59a97)](https://sonarqube.traincloud.net/dashboard?id=TrainCloud.HttpClient)
+
+## How to use
+Add the namespace `TrainCloud.HttpClient` to global usings
+
+**GlobalUsings.cs**
+```csharp
+using TrainCloud.HttpClient;
+```
+
+**_Imports.razor**
+```razor
+@using TrainCloud.HttpClient
+```
+
+### Example
+```csharp
+HttpClient client = ClientFactory.CreateClient("TrainCloudClient");
+
+ResponseModel? result = await client.PostRequestAsync<PostThingModel, ResponseModel>("/Route", validModel, httpStatus =>
+{
+    // Http-Status handling
+    if (httpStatus != System.Net.HttpStatusCode.OK)
+    {
+        ToastService.ShowWarning("Error message here");
+    }
+}, exception =>
+{
+    // Error
+    // ...
+});
+
+if (result is not null && ...)
+{
+    // ...
+}
+```
---
+++ TrainCloud.HttpClient/HttpClientExtensions.cs
@@ -0,0 +1,363 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace TrainCloud.HttpClient;
+
+/// <summary>
+/// This class extension provides a little more build out functionality for the HttpClient
+/// Send and receive POCOs with all de/serialization and Status/Error handling wrapped.
+/// The methods get used by the applications and the Api Test projects to make it work the same way in Dev/Test and Prod.
+/// </summary>
+public static class HttpClientExtensions
+{
+    /// <summary>
+    /// Global De/Serializer options for all Http requests in all TrainCloud projects
+    /// </summary>
+    private static JsonSerializerOptions TrainCloudJsonSerializerOptions { get; } = new() 
+    { 
+        PropertyNameCaseInsensitive = true
+    };
+
+    /// <summary>
+    /// Deserializes the Http response body into the requested type TResponse
+    /// </summary>
+    /// <typeparam name="TResponse">The requested type, to deserialize into.</typeparam>
+    /// <param name="response">The Http response</param>
+    /// <returns>The deserialized object as TResponse</returns>
+    private static async Task<TResponse?> DeserializeResponseBodyAsync<TResponse>(HttpResponseMessage response)
+    {
+        Stream contentStream = await response.Content.ReadAsStreamAsync();
+
+        TResponse? model = await JsonSerializer.DeserializeAsync<TResponse>(contentStream, TrainCloudJsonSerializerOptions);
+
+        return model;
+    }
+
+    /// <summary>
+    /// GETSs the ressource in requestUri and returns it as TResponse
+    /// </summary>
+    /// <typeparam name="TResponse">The Model type of the received ressource</typeparam>
+    /// <param name="client">Provides a class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.</param>
+    /// <param name="requestUri">The Uri of the ressource to work with.</param>
+    /// <param name="onHttpStatus">An action, which is raised after completing the request. Contains the actual Http Status code</param>
+    /// <param name="onException">An action, which is raised after an exception occurrs</param>
+    /// <returns>TResponse</returns>
+    public static async Task<TResponse?> GetRequestAsync<TResponse>(this System.Net.Http.HttpClient client, 
+                                                                    string requestUri, 
+                                                                    Action<HttpStatusCode>? onHttpStatus = null,
+                                                                    Action<Exception>? onException = null)
+    {
+        try
+        {
+            using HttpResponseMessage response = await client.GetAsync(requestUri);
+
+            if (onHttpStatus is not null)
+            {
+                onHttpStatus(response.StatusCode);
+            }
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return default;
+            }
+
+            TResponse? model = await DeserializeResponseBodyAsync<TResponse>(response);
+
+            return model;
+        }
+        catch (Exception ex)
+        {
+            if (onException is not null)
+            {
+                onException(ex);
+            }
+
+            return default;
+        }
+    }
+
+    /// <summary>
+    /// POSTSs the param at the ressource in requestUri and returns a TResponse
+    /// </summary>
+    /// <typeparam name="TPost">The type if the object which is sent as request body (param)</typeparam>
+    /// <typeparam name="TResponse">The Model type of the received ressource</typeparam>
+    /// <param name="client">Provides a class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.</param>
+    /// <param name="requestUri">The Uri of the ressource to work with.</param>
+    /// <param name="param">The object which is sent as request body</param>
+    /// <param name="onHttpStatus">An action, which is raised after completing the request. Contains the actual Http Status code</param>
+    /// <param name="onException">An action, which is raised after an exception occurrs</param>
+    /// <returns>TResponse</returns>
+    public static async Task<TResponse?> PostRequestAsync<TPost, TResponse>(this System.Net.Http.HttpClient client, 
+                                                                            string requestUri, 
+                                                                            TPost param, 
+                                                                            Action<HttpStatusCode>? onHttpStatus = null,
+                                                                            Action<Exception>? onException = null)
+    {
+        try
+        {
+            HttpContent parameterContent = JsonContent.Create(param, typeof(TPost));
+
+            using HttpResponseMessage response = await client.PostAsync(requestUri, parameterContent);
+
+            if (onHttpStatus is not null)
+            {
+                onHttpStatus(response.StatusCode);
+            }
+
+            if(!response.IsSuccessStatusCode)
+            {
+                return default;
+            }
+
+            TResponse? model = await DeserializeResponseBodyAsync<TResponse>(response);
+
+            return model;
+        }
+        catch (Exception ex)
+        {
+            if (onException is not null)
+            {
+                onException(ex);
+            }
+
+            return default;
+        }
+    }
+
+    /// <summary>
+    ///  POSTs the param at the ressource in requestUri
+    /// </summary>
+    /// <typeparam name="TPost">The type if the object which is sent as request body (param)</typeparam>
+    /// <param name="client">Provides a class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.</param>
+    /// <param name="requestUri">The Uri of the ressource to work with.</param>
+    /// <param name="param">The object which is sent as request body</param>
+    /// <param name="onHttpStatus">An action, which is raised after completing the request. Contains the actual Http Status code</param>
+    /// <param name="onException">An action, which is raised after an exception occurrs</param>
+    public static async Task PostRequestAsync<TPost>(this System.Net.Http.HttpClient client,
+                                                     string requestUri,
+                                                     TPost param,
+                                                     Action<HttpStatusCode>? onHttpStatus = null,
+                                                     Action<Exception>? onException = null)
+    {
+        try
+        {
+            HttpContent parameterContent = JsonContent.Create(param, typeof(TPost));
+
+            using HttpResponseMessage response = await client.PostAsync(requestUri, parameterContent);
+
+            if (onHttpStatus is not null)
+            {
+                onHttpStatus(response.StatusCode);
+            }
+        }
+        catch (Exception ex)
+        {
+            if (onException is not null)
+            {
+                onException(ex);
+            }
+        }
+    }
+
+    /// <summary>
+    /// PATCHes the param at the ressource in requestUri and returns a TResponse
+    /// </summary>
+    /// <typeparam name="TPatch">The type if the object which is sent as request body (param)</typeparam>
+    /// <typeparam name="TResponse">The Model type of the received ressource</typeparam>
+    /// <param name="client">Provides a class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.</param>
+    /// <param name="requestUri">The Uri of the ressource to work with.</param>
+    /// <param name="param">The object which is sent as request body</param>
+    /// <param name="onHttpStatus">An action, which is raised after completing the request. Contains the actual Http Status code</param>
+    /// <param name="onException">An action, which is raised after an exception occurrs</param>
+    /// <returns>TResponse</returns>
+    public static async Task<TResponse?> PatchRequestAsync<TPatch, TResponse>(this System.Net.Http.HttpClient client, 
+                                                                              string requestUri, 
+                                                                              TPatch param, 
+                                                                              Action<HttpStatusCode>? onHttpStatus = null,
+                                                                              Action<Exception>? onException = null)
+    {
+        try
+        {
+            HttpContent parameterContent = JsonContent.Create(param, typeof(TPatch));
+
+            using HttpResponseMessage response = await client.PatchAsync(requestUri, parameterContent);
+
+            if (onHttpStatus is not null)
+            {
+                onHttpStatus(response.StatusCode);
+            }
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return default;
+            }
+
+            TResponse? model = await DeserializeResponseBodyAsync<TResponse>(response);
+
+            return model;
+        }
+        catch (Exception ex)
+        {
+            if (onException is not null)
+            {
+                onException(ex);
+            }
+
+            return default;
+        }
+    }
+
+    /// <summary>
+    /// PATCHes the param at the ressource in requestUri
+    /// </summary>
+    /// <typeparam name="TPatch">The type if the object which is sent as request body (param)</typeparam>
+    /// <param name="client">Provides a class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.</param>
+    /// <param name="requestUri">The Uri of the ressource to work with.</param>
+    /// <param name="param">The object which is sent as request body</param>
+    /// <param name="onHttpStatus">An action, which is raised after completing the request. Contains the actual Http Status code</param>
+    /// <param name="onException">An action, which is raised after an exception occurrs</param>
+    public static async Task PatchRequestAsync<TPatch>(this System.Net.Http.HttpClient client,
+                                                       string requestUri,
+                                                       TPatch param,
+                                                       Action<HttpStatusCode>? onHttpStatus = null,
+                                                       Action<Exception>? onException = null)
+    {
+        try
+        {
+            HttpContent parameterContent = JsonContent.Create(param, typeof(TPatch));
+
+            using HttpResponseMessage response = await client.PatchAsync(requestUri, parameterContent);
+
+            if (onHttpStatus is not null)
+            {
+                onHttpStatus(response.StatusCode);
+            }
+        }
+        catch (Exception ex)
+        {
+            if (onException is not null)
+            {
+                onException(ex);
+            }
+        }
+    }
+
+    /// <summary>
+    /// PUTs the param at the ressource in requestUri and returns a TResponse
+    /// </summary>
+    /// <typeparam name="TPut"></typeparam>
+    /// <typeparam name="TResponse">The Model type of the received ressource</typeparam>
+    /// <param name="client">Provides a class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.</param>
+    /// <param name="requestUri">The Uri of the ressource to work with.</param>
+    /// <param name="param">The object which is sent as request body</param>
+    /// <param name="onHttpStatus">An action, which is raised after completing the request. Contains the actual Http Status code</param>
+    /// <param name="onException">An action, which is raised after an exception occurrs</param>
+    /// <returns>TResponse</returns>
+    public static async Task<TResponse?> PutRequestAsync<TPut, TResponse>(this System.Net.Http.HttpClient client,
+                                                                          string requestUri,
+                                                                          TPut param,
+                                                                          Action<HttpStatusCode>? onHttpStatus = null,
+                                                                          Action<Exception>? onException = null)
+    {
+        try
+        {
+            HttpContent parameterContent = JsonContent.Create(param, typeof(TPut));
+
+            using HttpResponseMessage response = await client.PutAsync(requestUri, parameterContent);
+
+            if (onHttpStatus is not null)
+            {
+                onHttpStatus(response.StatusCode);
+            }
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return default;
+            }
+
+            TResponse? model = await DeserializeResponseBodyAsync<TResponse>(response);
+
+            return model;
+        }
+        catch (Exception ex)
+        {
+            if (onException is not null)
+            {
+                onException(ex);
+            }
+
+            return default;
+        }
+    }
+
+    /// <summary>
+    /// PUTs the param at the ressource in requestUri
+    /// </summary>
+    /// <typeparam name="TPut">The type if the object which is sent as request body (param)</typeparam>
+    /// <param name="client">Provides a class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.</param>
+    /// <param name="requestUri">The Uri of the ressource to work with.</param>
+    /// <param name="param">The object which is sent as request body</param>
+    /// <param name="onHttpStatus">An action, which is raised after completing the request. Contains the actual Http Status code</param>
+    /// <param name="onException">An action, which is raised after an exception occurrs</param>
+    public static async Task PutRequestAsync<TPut>(this System.Net.Http.HttpClient client,
+                                                   string requestUri,
+                                                   TPut param,
+                                                   Action<HttpStatusCode>? onHttpStatus = null,
+                                                   Action<Exception>? onException = null)
+    {
+        try
+        {
+            HttpContent parameterContent = JsonContent.Create(param, typeof(TPut));
+
+            using HttpResponseMessage response = await client.PutAsync(requestUri, parameterContent);
+
+            if (onHttpStatus is not null)
+            {
+                onHttpStatus(response.StatusCode);
+            }
+        }
+        catch (Exception ex)
+        {
+            if (onException is not null)
+            {
+                onException(ex);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Deletes the ressource at the Uri specified in requestUri
+    /// </summary>
+    /// <param name="client">Provides a class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.</param>
+    /// <param name="requestUri">The Uri of the ressource to delete.</param>
+    /// <param name="onHttpStatus">An action, which is raised after completing the request. Contains the actual Http Status code</param>
+    /// <param name="onException">An action, which is raised after an exception occurrs</param>
+    /// <returns>True if success (Http 204 - NoContent), False if anything else</returns>
+    public static async Task<bool> DeleteRequestAsync(this System.Net.Http.HttpClient client, 
+                                                      string requestUri, 
+                                                      Action<HttpStatusCode>? onHttpStatus = null,
+                                                      Action<Exception>? onException = null)
+    {
+        try
+        {
+            using HttpResponseMessage response = await client.DeleteAsync(requestUri);
+
+            if (onHttpStatus is not null)
+            {
+                onHttpStatus(response.StatusCode);
+            }
+
+            return response.IsSuccessStatusCode;
+        }
+        catch(Exception ex)
+        {
+            if(onException is not null)
+            {
+                onException(ex);
+            }
+
+            return false;
+        }
+    }
+}
